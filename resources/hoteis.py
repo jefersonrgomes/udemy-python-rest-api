aa5bd155819b8a7406059998a3e0cf662b77b3b4
Jeferson Gomes <jeferson.rgomes@hotmail.com>
2024-03-24 19:53:31 +0000

    hotelsModel

--- resources/hoteis.py
+++ resources/hoteis.py
@@ -30,6 +30,24 @@ hoteis = [
     'cidade' : 'cidade 4'
     }
 ]
+
+class HotelModel:
+    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
+        self.hotel_id = hotel_id
+        self.nome = nome
+        self.estrelas = estrelas
+        self.diaria = diaria
+        self.cidade = cidade
+    def json(self):
+        return {
+            'hotel_id': self.hotel_id,
+            'nome': self.nome,
+            'estrelas': self.estrelas,
+            'diaria': self.diaria,
+            'cidade': self.cidade
+        }
+        
+
 class Hoteis(Resource):
     
    
@@ -60,14 +78,17 @@ class Hotel(Resource):
     #POST
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()       
-        novo_hotel = {'hotel_id' : hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()        
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
     #PUT
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = { 'hotel_id' : hotel_id, **dados }
+        
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hoteis.findHotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
